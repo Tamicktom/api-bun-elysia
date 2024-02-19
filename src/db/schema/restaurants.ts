9eafd838089a0b14a38e2eef5ab0a30436a59a54
Tamicktom <rickpokemon38@gmail.com>
2024-02-18 21:17:15 -0300

    update restaurants table

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -2,10 +2,15 @@
 import { text, timestamp, pgTable } from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
 
+import { users } from "./users";
+
 export const restaurants = pgTable("restaurants", {
   id: text("id").$defaultFn(createId).primaryKey(),
   name: text("name").notNull(),
   description: text("description"),
+  manager_id: text("manager_id").references(() => users.id, {
+    onDelete: "set null",
+  }),
   created_at: timestamp("created_at").notNull().defaultNow(),
   updated_at: timestamp("updated_at").notNull().defaultNow(),
 });
