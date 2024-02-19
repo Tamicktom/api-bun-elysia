d48d99d069e5f283d80e0501f0c23f3eec42f1d9
Tamicktom <rickpokemon38@gmail.com>
2024-02-18 21:21:54 -0300

    add relation to drizzle understand

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,7 +1,9 @@
 //* Libraries imports
 import { text, timestamp, pgTable } from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
+import { relations } from "drizzle-orm";
 
+//* Table imports
 import { users } from "./users";
 
 export const restaurants = pgTable("restaurants", {
@@ -13,4 +15,14 @@ export const restaurants = pgTable("restaurants", {
   }),
   created_at: timestamp("created_at").notNull().defaultNow(),
   updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+  return {
+    manager: one(users, {
+      fields: [restaurants.manager_id],
+      references: [users.id],
+      relationName: "restaurant_manager",
+    }),
+  };
 });
